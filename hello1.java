496d26a31e31ddca1be2ad316021cb6ce31252ac
Rohit Gaharwar <90830292+1234Rohit@users.noreply.github.com>
2023-10-31 11:58:52 +0530

    Add files via upload

---
+++ hello1.java
@@ -0,0 +1,6 @@
+class hello1{  
+    public static void main(String[] args){  
+    System.out.println("This is java app");  
+    System.out.println("This is java app by using Jenkins");  
+    }  
+    }  
