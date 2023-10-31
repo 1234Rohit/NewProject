31ffbf33e3d77894b9f801494517c4150b525eb7
Rohit Gaharwar <90830292+1234Rohit@users.noreply.github.com>
2023-10-31 12:24:35 +0530

    Update hello1.java

--- hello1.java
+++ hello1.java
@@ -1,6 +1,6 @@
 class hello1{  
     public static void main(String[] args){  
-    System.out.println("This is java app");  
+    System.out.println("This is java app demo ");  
     System.out.println("This is java app by using Jenkins");  
     }  
     }  
